c534df0c22de0170108b49c93dc24466cf38619c
DartSiders <mattheus_claubert@hotmail.com>
2024-12-20 22:54:55 -0300

    first commit

---
+++ javascript/ex001.js
@@ -0,0 +1,2 @@
+document.getElementById("texto").innerHTML =
+  "Olá, esse é meu primeiro texto <b>JS</b>!";
---
+++ módulo1/Ex018/capa.js
@@ -0,0 +1,4 @@
+const app = { 
+    to: "Recruiter",
+    msg: "Hire me, I need to pay the bills.😅"
+}
