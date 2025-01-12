2dd280d78bc77787fa23011d6f9035a6efb14df2
DartSiders <mattheus_claubert@hotmail.com>
2025-01-12 18:19:33 -0300

    update: exercicios

--- javascript/ex001.js
+++ javascript/ex001.js
@@ -1,2 +1,20 @@
-document.getElementById("texto").innerHTML =
-  "Olá, esse é meu primeiro texto <b>JS</b>!";
+// função que converte um inteiro em sua forma abreviada
+
+function abreviarNum(numero){
+  const unid = ["", "K","M","B","T"];
+  let index = 0;
+
+  while(numero >= 1000 && index < unid.length - 1){
+    numero /= 1000;
+    index++;
+}
+
+const inteiro = Math.floor(numero);;
+const decimal = Math.floor((numero - inteiro) * 10);
+
+return `${inteiro}${unid[index]}${decimal > 0 ? decimal : ''}`;
+}
+console.log(abreviarNum(150));
+console.log(abreviarNum(15000000));
+console.log(abreviarNum(150000));
+console.log(abreviarNum(1560));
